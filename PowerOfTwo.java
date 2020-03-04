6478c81ec32124f74908718a502101e6400f3a41
roshan1718 <shinderoshan.2902@gmail.com>
2020-03-04 06:56:52 +0530

    find powerof2 upto n number

---
+++ PowerOfTwo.java
@@ -0,0 +1,27 @@
+package BasicCore;
+
+public class PowerOfTwo {
+	
+		public static void main(String[] args) {
+
+		
+			int num, mul = 1;
+
+	
+			num = Integer.parseInt(args[0]);
+
+			
+			if (num > 31) {
+				System.out.println("value of number should be less than 31");
+				return;
+			}
+
+		
+			else {
+				System.out.println(mul);
+				for (int j = 1; j <= num; j++) {
+					mul = mul * 2;
+					System.out.println(mul);
+				}
+			}
+}}
